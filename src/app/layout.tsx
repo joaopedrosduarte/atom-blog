df7b38086c5df88edf5b28c217fd8aeb1ffd7970
joaopedrosduarte <jpds@cesar.school>
2024-02-29 23:59:39 -0300

    Add comments and change app port to 3001

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
+    // Normalmente o header estaria nesse componente para ser renderizado 
+    // em todas as paginas Porém como isso desabilitaria o hot reload, 
+    // tirando as animações (não sei se tem um jeito de fazer assim).
+    // Assim header foi colocado em page.tsx.
     <html lang="en" className={`${roboto.variable} ${poppins.variable}`}>
       <body>{children}</body>
     </html>
