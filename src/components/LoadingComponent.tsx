3c6b3dab7ab49fda36f2eb6f579eb09cb6b41a9a
joaopedrosduarte <jpds@cesar.school>
2024-03-01 09:00:56 -0300

    Adjust responsiviness on Loading, NoPosts and Error components

--- src/components/LoadingComponent.tsx
+++ src/components/LoadingComponent.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const LoadingComponent = () => {
   return (
-    <div className="flex flex-col px-64 py-44 w-full items-center justify-between">
+    <div className="flex flex-col px-auto py-44 w-full items-center justify-between">
       <div className="gap-4 w-full flex flex-col items-center justify-between">
         <div className="flex animate-spin">
           <Image src={"/spinner.svg"} alt="Spinner" width={96} height={96} />
--- src/components/NoPostsComponent.tsx
+++ src/components/NoPostsComponent.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const NoPostsComponent = () => {
   return (
-    <div className="flex flex-col px-64 py-44 w-full items-center justify-between">
+    <div className="flex flex-col px-auto py-44 w-full items-center justify-between">
       <div className="gap-4 w-full flex flex-col items-center justify-between">
         <Image src={"/broom.svg"} alt="Spinner" width={96} height={96} />
         <span className="text-2xl font-bold text-slate-300 block w-46 text-center">
